4f151c2857319bb87c9cd4ee7ec8ae26a201a798
KarlStarkweather <stark13@usa.net>
2022-04-09 16:54:09 -0700

    add model for flavor

---
+++ PierresTreats/Models/Flavor.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class Flavor
+  {
+
+    public Flavor()
+    {
+      this.JoinPierresTreats = new HashSet<TreatFlavor>();
+    }
+
+    public int FlavorId { get; set; }
+    public string Name { get; set; }
+
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<TreatFlavor> JoinPierresTreats { get; set; }
+  }
+}
