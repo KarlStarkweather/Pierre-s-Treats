50ba4b9f16f27f3a682c277a942d0727ac5983ba
KarlStarkweather <stark13@usa.net>
2022-04-09 17:00:46 -0700

    add join model and authentication for treats

---
+++ PierresTreats/Models/FlavorTreat.cs
@@ -0,0 +1,13 @@
+namespace PierresTreats.Models
+{
+  public class FlavorTreat
+  {
+    public int FlavorTreatId { get; set; }
+    public int TreatId { get; set; }
+    public int FlavorId { get; set; }
+
+    public virtual Treat Treat { get; set; }
+    public virtual Flavor Flavor { get; set; }
+    public virtual ApplicationUser User { get; set; }
+  }
+}
